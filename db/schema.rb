8ff12df429a52aa9b1bb02080e010a33b52087bc
yosefs <yosef.s@highgearmedia.com>
2012-08-20 00:17:10 +0300

    add seeds.db test data for db

---
+++ db/schema.rb
@@ -0,0 +1,37 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20120819190044) do
+
+  create_table "cities", :force => true do |t|
+    t.string  "name"
+    t.decimal "latitude",  :precision => 15, :scale => 10, :default => 0.0
+    t.decimal "longitude", :precision => 15, :scale => 10, :default => 0.0
+  end
+
+  add_index "cities", ["name"], :name => "index_cities_on_name", :unique => true
+
+  create_table "tagcities", :force => true do |t|
+    t.integer "city_id"
+    t.integer "tag_id"
+  end
+
+  add_index "tagcities", ["city_id", "tag_id"], :name => "index_tagcities_on_city_id_and_tag_id", :unique => true
+
+  create_table "tags", :force => true do |t|
+    t.string "name"
+  end
+
+  add_index "tags", ["name"], :name => "index_tags_on_name", :unique => true
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+City.create([
+{name: 'Colorado',latitude: 38.891033,longtitude: -104.414062},
+{name: 'Manitoba',latitude: 56.12106,longtitude: -97.998047},
+{name: 'colorado',latitude: 38.891033,longtitude: -104.414062},
+{name: 'Jerusalem',latitude: 31.774878,longtitude: 35.14801},
+{name: 'Ashdod',latitude: 31.798224,longtitude: 34.642639},
+{name: 'Kiryat Gat',latitude: 31.604271,longtitude: 34.763489},
+{name: 'Beit Shemesh',latitude: 31.737511,longtitude: 34.991455},
+{name: 'Rishon LeZion',latitude: 31.954493,longtitude: 34.799194},
+{name: 'Netanya',latitude: 32.324276,longtitude: 34.859619},
+{name: 'Hadera',latitude: 32.440249,longtitude: 34.900818},
+{name: 'Beersheba',latitude: 31.257422,longtitude: 34.755249}])
+
+Tag.create([
+{name: 'old'},
+{name: 'new'},
+{name: 'great'},
+{name: 'small'},
+{name: 'cool'},
+{name: 'culture'},
+{name: 'big'}])
+
+
+Tagcity.create([
+{city_id: 1,tag_id: 2},
+{city_id: 1,tag_id:  4},
+{city_id: 1,tag_id:  3},
+{city_id: 2,tag_id:  1},
+{city_id: 3,tag_id:  4},
+{city_id: 4,tag_id:  3},
+{city_id: 4,tag_id: 1},
+{city_id: 5,tag_id:  2},
+{city_id: 5,tag_id: 5},
+{city_id: 6,tag_id:  1}])
