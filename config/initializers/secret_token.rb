6eb0f68e0b871512951a5ad514be756992b5d199
yosefs <yosef.s@highgearmedia.com>
2012-08-19 21:16:37 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Citytag::Application.config.secret_token = '35aaac1fa2fcb0ae83ef4087222c270bb592404a020cebd30121520d10f4041236a5c90370f0f78576f5d6d264655400fd36d7dfcd75f9bb72740d84b9a2a21d'
